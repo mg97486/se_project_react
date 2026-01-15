b4c4ab342f6cca43ffe2529f6d288874202f05e1
Matthew <smearygeary613@gmail.com>
2026-01-15 12:27:26 -0700

    fixes to project

--- src/components/logInModal/logInModal.jsx
+++ src/components/logInModal/logInModal.jsx
@@ -1,7 +1,7 @@
 import { useForm } from "../../hooks/useForm";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import { NavLink } from "react-router-dom";
-import "./LoginModal.css";
+import "./loginModal.css";
 
 const LogInModal = ({ isOpen, onClose }) => {
   const defaultValues = { email: "", password: "" };
